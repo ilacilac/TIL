204ca9b9a00f81125b133fc94d3eab150e5cdb9b
ilacilac <qkralswl307@gmail.com>
2020-05-11 19:34:19 +0900

    docs: binarySearch

--- javaScript/FC_FDS_16/test/Exercise_03.js
+++ javaScript/FC_FDS_16/test/Exercise_03.js
@@ -6,33 +6,43 @@
   검색 대상 값을 검색할 때까지 이와 같은 처러를 반복한다.
 */
 
-/*
-1. target이랑 mid랑 비교한다. 같으면 mid를 return
-2. 부등호 방향이 향한 방향으로 array length / 2한것에 다시 /2한다.
-3. 만약 부등호 방향이 target 더 크면 array의 마지막 index보단 작고 mid보단 크다
-4. target이랑 index가 맞을때 return
-array[i]랑 target이랑 일치할때 index = array[i]
-
-while은 어디다쓰지
-index = 0일때
-이진검색 조건에 안맞으면 ?
-나누어질때마다 while을 돌까? -> while 이 어느시점에서 쓰여야하는지 모르겠다 ; ㅅ;
-*/
-
-const target = 15; // 임의로 지정한 타겟
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]; // 임의로 지정한 배열
-let start = 1;
-let end = array.length; // 20
-let mid = Math.floor((start + end) / 2); // 10
-let index = -1; // 타겟이 배열안에 없을경우 return 해줄 값
-
-function binarySearch() {
-  if (target > mid) { // 15 > 10
-    start = mid;
-    console.log(start); // 10
-    if (target > start) {
-      // start = mid + ()
+// eslint-disable-next-line consistent-return
+function binarySearch(array, target) {
+  let start = array[0];
+  let end = array[array.length - 1]; // 20
+  while (start !== end) {
+    let mid = Math.floor((start + end) / 2); // 10
+    // target이 mid일 경우
+    if (mid === target) {
+      return mid - 1; // index
+    }
+    // target이 mid 보다 클 경우
+    if (mid < target) {
+      start = mid + 1;
+      if (start === end) {
+        return (mid);
+      }
+      if (start >= end || target > end) {
+        return -1;
+      }
+    }
+    // target이 mid 보다 작을 경우
+    if (mid > target) {
+      end = mid - 1;
+      if (start === end) {
+        return mid;
+      }
+      if (start >= end || start > target) {
+        return -1;
+      }
     }
   }
 }
-binarySearch();
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
