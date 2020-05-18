254faf7cf0b41d1eac44b48605af2169116562e0
ilacilac <qkralswl307@gmail.com>
2020-05-18 20:43:03 +0900

    docs: Invert 'Hello world!' and Print

---
+++ javaScript/FC_FDS_16/test/Exercise_05.js
@@ -0,0 +1,6 @@
+const words = 'Hello world!'.split('');
+console.log(words.reverse().join(''));
+
+// 풀이
+// let words = 'Hello world!'.split('').reverse().join('');
+// [... 'Hello world!'].reverse().join(''); // [!dlro]
