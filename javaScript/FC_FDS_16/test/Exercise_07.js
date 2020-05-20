4361e9028cd492edb054feb079abec9470ecc6d4
ilacilac <qkralswl307@gmail.com>
2020-05-20 10:32:57 +0900

    docs: Bubble sort

--- javaScript/FC_FDS_16/test/Exercise_07.js
+++ javaScript/FC_FDS_16/test/Exercise_07.js
@@ -8,18 +8,18 @@
 */
 
 function bubbleSort(array) {
-    for (let i = (array.length - 1); i > 0; i--) {
-        for (let j = 0; j < i; j++) {
-            let temp = array[j];
-            if (array[j] > array[j + 1]) {
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
+  for (let i = (array.length - 1); i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      let temp = array[j];
+      if (array[j] > array[j + 1]) {
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
-    return array;
+  }
+  return array;
 }
 
-console.log(bubbleSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
-console.log(bubbleSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
+console.log(bubbleSort([2, 4, 5, 1, 3])); // [1, 2, 3, 4, 5]
+console.log(bubbleSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
 console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
