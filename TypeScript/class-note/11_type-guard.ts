ecb513b7eb93b3aea5896752996f487239c3fdbd
ilacilac <qkralswl307@gmail.com>
2020-12-14 23:04:16 +0900

    docs: Type guard

--- TypeScript/class-note/11_type-guard.ts
+++ TypeScript/class-note/11_type-guard.ts
@@ -15,13 +15,13 @@ var tony = introduce();
 // console.log(tony.skill);
 
 // 타입 단언만 사용 시, 가독성이 떨어지므로 아래와같이 쓰지말자 => 타입가드를 사용하자
-if ((tony as Developer).skill) {
-  var skill = (tony as Developer).skill;
-  console.log(skill);
-} else if ((tony as Person).age) {
-  var age = (tony as Person).age;
-  console.log(age);
-}
+// if ((tony as Developer).skill) {
+//   var skill = (tony as Developer).skill;
+//   console.log(skill);
+// } else if ((tony as Person).age) {
+//   var age = (tony as Person).age;
+//   console.log(age);
+// }
 
 // 타입 가드 정의
 function isDeveloper(target: Developer | Person): target is Developer {
