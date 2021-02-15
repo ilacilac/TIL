354d06ac96a68e88cd28113009b25ea3713fcf03
ilacilac <qkralswl307@gmail.com>
2021-02-15 21:47:45 +0900

    feat: game & loading exercise

--- TypeScript/class-note/2-types-projects/game.ts
+++ TypeScript/class-note/2-types-projects/game.ts
@@ -1,6 +1,33 @@
 /**
  * Let's make a game 🕹
  */
+interface Pos {
+  x: number;
+  y: number
+}
+const position:Pos = {
+  x: 0,
+  y: 0
+}
+
+function move(direction: 'up' | 'down' | 'left' | 'right') {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`Unknown direction ${direction}`);
+  }
+}
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
