fc05bba12c77efe027f2bc6d4c6e1861833377c3
ilacilac <qkralswl307@gmail.com>
2021-09-15 13:11:47 +0900

    Feat: Gnb

--- react/nextjs-tutorial/src/component/Top.js
+++ react/nextjs-tutorial/src/component/Top.js
@@ -1,7 +1,10 @@
+import Gnb from "./Gnb";
+
 export default function Top() {
   return (
     <header>
       <h1><img src="/images/lilac-icon.png" alt="logo" width="40" /></h1>
+      <Gnb />
     </header>
   )
 }
