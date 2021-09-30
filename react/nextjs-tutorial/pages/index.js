61d49dca6770d47d51c879faaa5d8e058ee744d5
ilacilac <qkralswl307@gmail.com>
2021-09-30 20:28:37 +0900

    Feat: GNB

--- react/nextjs-tutorial/pages/index.js
+++ react/nextjs-tutorial/pages/index.js
@@ -3,18 +3,13 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
   return (
     <div>
-      create-next-app 으로 설치면
-      <br />
-      1. 컴파일과 번들링이 자동으로 된다. (webpack, babel)
-      <br />
-      2. 자동 리프레쉬 기능으로 수정하면 화면에 바로 반영.
-      <br />
-      3. 서버사이드 렌더링이 지원된다.
-      <br />
-      4. 스태틱 파일을 지원.
-      <br />
+      <Head>
+        <title>HOME | ilacilac</title>
+      </Head>
     </div>
   );
 }
--- react/nextjs-tutorial/src/component/Gnb.js
+++ react/nextjs-tutorial/src/component/Gnb.js
@@ -1,32 +1,27 @@
-import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import React from "react";
+import { Menu } from "semantic-ui-react";
 
 export default function Gnb() {
-  return(
+  const activeItem = "home";
+  return (
     <Menu>
-        <Menu.Item
-          name='editorials'
-          // active={activeItem === 'editorials'}
-          // onClick={this.handleItemClick}
-        >
-          Editorials
-        </Menu.Item>
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        // onClick={this.handleItemClick}
+      />
 
-        <Menu.Item
-          name='reviews'
-          // active={activeItem === 'reviews'}
-          // onClick={this.handleItemClick}
-        >
-          Reviews
-        </Menu.Item>
+      <Menu.Item
+        name="messages"
+        active={activeItem === "messages"}
+        // onClick={this.handleItemClick}
+      />
 
-        <Menu.Item
-          name='upcomingEvents'
-          // active={activeItem === 'upcomingEvents'}
-          // onClick={this.handleItemClick}
-        >
-          Upcoming Events
-        </Menu.Item>
-      </Menu>
-  )
+      <Menu.Item
+        name="freinds"
+        active={activeItem === "freinds"}
+        // onClick={this.handleItemClick}
+      />
+    </Menu>
+  );
 }
