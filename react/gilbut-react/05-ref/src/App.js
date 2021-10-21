5150e7c0e3c2c2c113952560a839849a77c9caa8
ilacilac <qkralswl307@gmail.com>
2021-10-21 14:57:07 +0900

    Feat: Using ref

---
+++ react/gilbut-react/05-ref/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+
+function App() {
+  return (
+    <>
+      <ValidationSample />
+      <ScrollBox />
+    </>
+  );
+}
+
+export default App;
---
+++ react/gilbut-react/05-ref/src/ScrollBox.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class ScrollBox extends Component {
+  render() {
+    const style = {
+      border: '1px solid #000',
+      height: '300px',
+      width: '300px',
+      overflow: 'auto',
+      position: 'relative'
+    };
+
+    const innerStyle = {
+      width: '100%',
+      height: '650px',
+      background: 'linear-gradient(white, black)'
+    }
+
+    return (
+      <div
+        style={style}
+        ref={(ref) => this.box = ref}
+      >
+        <div style={innerStyle} />
+      </div>
+    )
+  }
+}
+
+export default ScrollBox
+
