c9878ca6e0322a62b410c9a1392991fa7d442718
ilacilac <qkralswl307@gmail.com>
2021-11-03 15:51:30 +0900

    Feat: TodoInsert in TodoTemplate

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import styled from 'styled-components';
-import TodoInsert from './components/TodoInsert';
 
 const GlobalStyle = styled.div`
   height: 100vh;
@@ -11,47 +10,10 @@ const GlobalStyle = styled.div`
   flex-direction: column;
 `;
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, content: 'Studying', done: false },
-    { id: 2, content: 'Working out', done: true },
-    { id: 3, content: 'Reading a book', done: false },
-  ]);
-
-  const [content, setContent] = useState('');
-
-  const id = Math.max(...todos.map((todo) => todo.id)) + 1;
-
-  const changingDone = (e, id) => {
-    const done = e.currentTarget.checked;
-    const newTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, done };
-      } else {
-        return todo;
-      }
-    });
-    setTodos(newTodos);
-  };
-
-  const insertTodo = () => {
-    setTodos([
-      ...todos,
-      {
-        id,
-        content,
-        done: false,
-      },
-    ]);
-  };
-
-  const onChange = (e) => {
-    setContent(e.currentTarget.value);
-  };
+const App = () => { 
   return (
     <GlobalStyle>
-      <TodoInsert insertTodo={insertTodo} onChange={onChange} />
-      <TodoTemplate todos={todos} changingDone={changingDone} />
+      <TodoTemplate />
     </GlobalStyle>
   );
 };
--- react/todo-app/src/components/TodoTemplate.js
+++ react/todo-app/src/components/TodoTemplate.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
+import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 
 const TodoTemplateStyle = styled.div`
@@ -7,15 +8,52 @@ const TodoTemplateStyle = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  width: 50%;
+  width: 30%;
   height: 450px;
   padding: 50px;
   background: #ffffff;
 `;
 
-const TodoTemplate = ({ todos, changingDone }) => {
+const TodoTemplate = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, content: 'Studying', done: false },
+    { id: 2, content: 'Working out', done: true },
+    { id: 3, content: 'Reading a book', done: false },
+  ]);
+
+  const [content, setContent] = useState('');
+
+  const id = Math.max(...todos.map((todo) => todo.id)) + 1;
+
+  const changingDone = useCallback((e, id) => {
+    const done = e.currentTarget.checked;
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, done };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodos);
+  },[todos]);
+
+  const insertTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id,
+        content,
+        done: false,
+      },
+    ]);
+  };
+
+  const onChange = (e) => {
+    setContent(e.currentTarget.value);
+  };
   return (
     <TodoTemplateStyle>
+      <TodoInsert insertTodo={insertTodo} onChange={onChange} />
       <TodoList todos={todos} changingDone={changingDone} />
     </TodoTemplateStyle>
   );
