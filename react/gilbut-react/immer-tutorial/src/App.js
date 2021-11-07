4c9e4d3bcb21edd607bb9e21c3edd11bca359535
layla <layla@cizion.com>
2021-11-08 00:48:37 +0900

    Feat: immer

---
+++ react/gilbut-react/immer-tutorial/src/App.js
@@ -0,0 +1,123 @@
+import React, { useCallback, useRef, useState } from "react";
+import produce from "immer";
+
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    // setForm({
+    //   ...form,
+    //   [name]: [value],
+    // });
+
+    // setForm(
+    //   produce(form, (draft) => {
+    //     draft[name] = value;
+    //   })
+    // );
+
+    setForm(
+      produce((draft) => {
+        draft[name] = value;
+      })
+    );
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      // setData({
+      //   ...data,
+      //   array: data.array.concat(info),
+      // });
+
+      // setData(
+      //   produce(data, (draft) => {
+      //     draft.array.push(info);
+      //   })
+      // );
+
+      setData(
+        produce((draft) => {
+          draft.array.push(info);
+        })
+      );
+      setForm({
+        name: "",
+        username: "",
+      });
+
+      nextId.current += 1;
+    },
+    [form.name, form.username]
+  );
+
+  const onRemove = useCallback((id) => {
+    // setData({
+    //   ...data,
+    //   array: data.array.filter((info) => info.id !== id),
+    // });
+
+    // setData(
+    //   produce(data, (draft) => {
+    //     draft.array.splice(
+    //       draft.array.findIndex((info) => info.id === id),
+    //       1
+    //     );
+    //   })
+    // );
+
+    setData(
+      produce((draft) => {
+        draft.array.splice(draft.array.findIndex((info) => info.id === id, 1));
+      })
+    );
+  }, []);
+
+  console.log("form", form);
+  console.log("data", data);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
