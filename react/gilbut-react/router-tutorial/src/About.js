4e5d9aa8be41e0575e07fe420b19a8caadf9b40b
ilacilac <qkralswl307@gmail.com>
2021-11-10 10:04:58 +0900

    Feat: Using useLocation

--- react/gilbut-react/router-tutorial/src/About.js
+++ react/gilbut-react/router-tutorial/src/About.js
@@ -1,10 +1,22 @@
 import React from "react";
+import qs from 'qs';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
 
 const About = () => {
+  // const params = useParams();
+  // const navigate = useNavigate();
+  const location = useLocation();
+
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const showDetail = query.detail === 'true';
+
   return (
     <div>
       <h1>소개</h1>
       <p>소개 페이지</p>
+      {showDetail && <p>detail값을 true로 설정하셨군용</p>}
     </div>
   );
 };
