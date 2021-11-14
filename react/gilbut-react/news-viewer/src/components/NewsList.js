c87b76d31550d1f00289e1745c2510a4bccfe347
ilacilac <qkralswl307@gmail.com>
2021-11-14 20:45:08 +0900

    Feat: category link

--- react/gilbut-react/news-viewer/src/components/NewsList.js
+++ react/gilbut-react/news-viewer/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=70a092f6dfe0483da14623916cdb7671',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=70a092f6dfe0483da14623916cdb7671`,
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -34,7 +35,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기중..</NewsListBlock>;
