3a0544e53a58b9668d5c2f0bb2f3a4f686fd83e6
ilacilac <qkralswl307@gmail.com>
2021-11-16 21:43:38 +0900

    Style: UI

--- react/gilbut-react/context-tutorial/src/contexts/color.js
+++ react/gilbut-react/context-tutorial/src/contexts/color.js
@@ -1,5 +1,28 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const ColorContext = createContext({ color: 'black' });
+const ColorContext = createContext({
+  state: { color: 'black', subColor: 'red' },
+  actions: {
+    setColor: () => {},
+    setSubColor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState('black');
+  const [subColor, setSubColor] = useState('red');
+
+  const value = {
+    state: { color, subColor },
+    actions: { setColor, setSubColor },
+  };
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+const { Consumer: ColorConsumer } = ColorContext;
+
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
