62342264ca4182104bdbcfc37e1aefdac6ec859d
ilacilac <qkralswl307@gmail.com>
2021-12-07 22:42:36 +0900

    Feat: use React.lazy & suspense

--- react/gilbut-react/spliting-sample/src/App.js
+++ react/gilbut-react/spliting-sample/src/App.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react'
+import React, { Suspense, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
-export class App extends Component {
-  state = {
-    SplitMe: null
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   }
-  handleClick = async () => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default
-    });
-  }
-  render() {
-    const {SplitMe} = this.state;
-    return (
-      <div className="App">
+  return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handleClick}>Hello React</p> 
-        {SplitMe && <SplitMe />}
+        <p onClick={onClick}>Hello React</p> 
+        <Suspense fallback={<div>loading....</div>}>
+            {visible && <SplitMe />}
+        </Suspense>
       </header>
     </div>
-    )
-  }
+  )
 }
 
-export default App;
+export default App
