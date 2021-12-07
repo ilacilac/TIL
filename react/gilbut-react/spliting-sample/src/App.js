d64fd2587e9579391326c7364cd17115df3b33f6
ilacilac <qkralswl307@gmail.com>
2021-12-07 22:46:46 +0900

    Feat: use @loadable/component

--- react/gilbut-react/spliting-sample/src/App.js
+++ react/gilbut-react/spliting-sample/src/App.js
@@ -1,21 +1,27 @@
 import React, { Suspense, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+import loadable from '@loadable/component';
+
+// const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   }
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React</p> 
-        <Suspense fallback={<div>loading....</div>}>
-            {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React</p> 
+        {visible && <SplitMe />}
       </header>
     </div>
   )
