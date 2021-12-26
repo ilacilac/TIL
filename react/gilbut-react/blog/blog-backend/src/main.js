dec3bbd6e7ad3e8ea7a699ffbe99eb6e01a9d15f
ilacilac <qkralswl307@gmail.com>
2021-12-26 22:37:45 +0900

    Feat: Post data, pagination, limit

--- react/gilbut-react/blog/blog-backend/src/main.js
+++ react/gilbut-react/blog/blog-backend/src/main.js
@@ -4,6 +4,7 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api';
+import createFakeData from './createFakeData';
 
 // eslint-disable-next-line no-undef
 const { PORT, MONGO_URI } = process.env;
@@ -12,6 +13,7 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to MongoDB');
+    createFakeData();
   })
   .catch((e) => {
     console.error(e);
