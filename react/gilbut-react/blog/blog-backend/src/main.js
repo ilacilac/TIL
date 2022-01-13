27fcb6543ee6570f7ce4f0b7e1b1b29e2018ffea
ilacilac <qkralswl307@gmail.com>
2022-01-13 15:54:25 +0900

    Feat: apply build static file

--- react/gilbut-react/blog/blog-backend/src/main.js
+++ react/gilbut-react/blog/blog-backend/src/main.js
@@ -4,6 +4,10 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
 
@@ -30,6 +34,15 @@ app.use(jwtMiddleware);
 router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
+
+// eslint-disable-next-line no-undef
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
