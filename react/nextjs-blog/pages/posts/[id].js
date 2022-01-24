b435e9413e40d764ab4616b20b6d0a5c2432ce61
ilacilac <qkralswl307@gmail.com>
2022-01-24 14:59:53 +0900

    Feat: Rendering MD content

--- react/nextjs-blog/pages/posts/[id].js
+++ react/nextjs-blog/pages/posts/[id].js
@@ -12,7 +12,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({params}) {
   // params : { id: 'pre-rendering' }
   console.log('params', params);
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData
@@ -28,6 +28,8 @@ export default function Post({postData}) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
     </Layout>
   )
 }
