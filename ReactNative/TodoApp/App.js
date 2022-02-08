53f77c497e624a7524255915aefbdc6167aea38c
ilacilac <qkralswl307@gmail.com>
2022-02-08 11:07:02 +0900

    Feat: Add / Toggle todo

--- ReactNative/TodoApp/App.js
+++ ReactNative/TodoApp/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   KeyboardAvoidingView,
   Platform,
@@ -10,9 +10,36 @@ import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 const App = () => {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
+  const onInsert = text => {
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -22,8 +49,12 @@ const App = () => {
           behavior={Platform.select({ios: 'padding', android: undefined})}
           style={styles.avoid}>
           <DateHead date={today} />
-          <Empty />
-          <AddTodo />
+          {todos.length === 0 ? (
+            <Empty />
+          ) : (
+            <TodoList todos={todos} onToggle={onToggle} />
+          )}
+          <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
---
+++ ReactNative/TodoApp/components/TodoItem.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+function TodoItem({id, text, done, onToggle}) {
+  console.log(id);
+  return (
+    <View style={styles.item}>
+      <TouchableOpacity onPress={() => onToggle(id)}>
+        <View style={[styles.circle, done && styles.filled]}>
+          {done && (
+            <Image
+              source={require('../assets/icons/check_white/check_white.png')}
+            />
+          )}
+        </View>
+      </TouchableOpacity>
+      <Text style={[styles.text, done && styles.lineThrough]}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    padding: 16,
+    alignItems: 'center',
+  },
+  circle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    borderColor: '#26a69a',
+    borderWidth: 1,
+    marginRight: 16,
+  },
+  filled: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#26a69a',
+  },
+  text: {
+    flex: 1,
+    fontSize: 16,
+    color: '#212121',
+  },
+  lineThrough: {
+    color: '#9e9e9e',
+    textDecorationLine: 'line-through',
+  },
+});
+
+export default TodoItem;
